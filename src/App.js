86768e377690ae911d52a8345af9f0ce14fed68c
Leandro <officialdevmi@gmail.com>
2021-11-02 07:54:29 -0700

    Change App component and add store to index

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import MainPage from './components/MainPage';
+import SecondPage from './components/SecondPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <div className="Bookstore-CMS">
+            <MainPage />
+          </div>
+        </Route>
+        <Route path="/categories">
+          <div className="Bookstore-CMS">
+            <SecondPage />
+          </div>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
