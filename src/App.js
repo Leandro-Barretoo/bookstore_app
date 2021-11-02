9187c794651ea284971f7ce14a69f3df8f65d8f3
Leandro <officialdevmi@gmail.com>
2021-11-02 08:15:14 -0700

    Add react router depedencies

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import './App.css';
 import MainPage from './components/MainPage';
 import SecondPage from './components/SecondPage';
 
