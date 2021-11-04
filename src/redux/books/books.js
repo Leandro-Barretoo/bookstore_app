9b7438df29f7a917b9ba06d80d9da6c79eb9c3f6
Leandro-Barretoo <officialdevmi@gmail.com>
2021-11-04 09:04:39 -0700

    Add add action creator

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,7 +1,19 @@
+const ADD_BOOK = 'bookStore/books/ADD_BOOK';
+
 const initialState = [];
 
+export const addBook = (payload) => ({
+  type: ADD_BOOK,
+  payload,
+});
+
 const booksReducer = (state = initialState, action) => {
-  return state;
+  switch (action.type) {
+    case ADD_BOOK:
+      return [...state, action.payload];
+    default:
+      return state;
+  }
 };
 
 export default booksReducer;
