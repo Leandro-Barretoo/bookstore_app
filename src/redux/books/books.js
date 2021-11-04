1b60a9ee4ed12db1b582f03fa47a1eb735dd35c4
Leandro-Barretoo <officialdevmi@gmail.com>
2021-11-04 09:07:33 -0700

    Add remove action creator

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,4 +1,5 @@
 const ADD_BOOK = 'bookStore/books/ADD_BOOK';
+const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
 
 const initialState = [];
 
@@ -7,10 +8,17 @@ export const addBook = (payload) => ({
   payload,
 });
 
+export const removeBook = (payload) => ({
+  type: REMOVE_BOOK,
+  payload,
+});
+
 const booksReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_BOOK:
       return [...state, action.payload];
+    case REMOVE_BOOK:
+      return state.filter((book) => book.id !== action.payload);
     default:
       return state;
   }
