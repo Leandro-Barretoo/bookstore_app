6ab13da9a0116492f427e5481498298d423e3b23
Leandro-Barretoo <officialdevmi@gmail.com>
2021-11-05 08:09:08 -0700

    Add styles to Nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Nav.css';
 import NavLink from './NavLink';
 
 const Nav = () => (
-  <div>
+  <div className="NavBar">
     <NavLink title="Bookstore CMS" />
     <NavLink title="Books" />
     <NavLink title="Categories" />
