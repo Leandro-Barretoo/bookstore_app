29548561baa08bd9468a5dd63ebc840fbdf6dea7
Leandro-Barretoo <officialdevmi@gmail.com>
2021-11-05 08:10:33 -0700

    Add styles to Navigation Links

--- src/components/NavLink.js
+++ src/components/NavLink.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './NavLink.css';
 import { Link } from 'react-router-dom';
 
 const NavLink = (props) => {
   const { title } = props;
   switch (title) {
     case 'Bookstore CMS':
-      return <span><Link to="/">{title}</Link></span>;
+      return <span><Link to="/" className="Logo">{title}</Link></span>;
     case 'Books':
-      return <span><Link to="/">{title}</Link></span>;
+      return <span><Link to="/" className="Bok-Cat Cat-Op">{title}</Link></span>;
     case 'Categories':
-      return <span><Link to="/categories">{title}</Link></span>;
+      return <span><Link to="/categories" className="Cat-Cat Cat-Op">{title}</Link></span>;
     default:
       return <span>{title}</span>;
   }
