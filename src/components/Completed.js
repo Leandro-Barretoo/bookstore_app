717dcb1e95604e104bcade386d6afa73613cd9f6
Leandro-Barretoo <officialdevmi@gmail.com>
2021-11-05 08:03:29 -0700

    Add style to completion information

--- src/components/Completed.js
+++ src/components/Completed.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import './Completed.css';
 
 const Completed = () => (
-  <div>
+  <div className="Comp-Container">
     <div className="Regtangle-3">
       <div className="Oval-2" />
     </div>
-    <div>
-      <span>64%</span>
-      <span>Completed</span>
+    <div className="Percentage-Comp">
+      <span className="Percentage Display-Block Family-Montserrat">64%</span>
+      <span className="Comp Display-Block Family-Montserrat">Completed</span>
     </div>
   </div>
 );
